664e2dd9f03982dd8b1c3d399fb47350dcc15de8
polatefekaya <polatsfekaya@gmail.com>
2026-05-12 16:09:37 +0300

    f

--- src/components/navbar/KzLogo.tsx
+++ src/components/navbar/KzLogo.tsx
@@ -25,7 +25,6 @@ export const KzLogo: Component<KzLogoProps> = (props) => {
         viewBox="0 0 127.267 126.507"
         width="288"
         height="288"
-        transform="matrix(2.262959006398843,0,0,2.262959006398843,112.6630554199219,104.54794129739156)"
       >
         <defs>
           <clipPath id={clipB} clipPathUnits="userSpaceOnUse">
